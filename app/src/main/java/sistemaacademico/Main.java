bc1f1b29fbe5e7ec4666ec847287bacdb63d1d01
carlos daniel ospino <carlosdospino@unicesar.edu.co>
2026-03-02 01:04:44 -0500

    feat: crear Main.java y organizar estructura

---
+++ app/src/main/java/sistemaacademico/Main.java
@@ -0,0 +1,16 @@
+package sistemaacademico;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    
+    static ArrayList<Asignatura> asignaturas = new ArrayList<>();
+    static ArrayList<Nota> notas = new ArrayList<>();
+    
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        System.out.println("Sistema funcionando");
+    }
+}
