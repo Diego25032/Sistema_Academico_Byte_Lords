9e92d2c22c548bed571a5556f458e59b3feea355
icamilohernandez <icamilohernandez@unicesar.edu.co>
2026-03-01 22:49:13 -0500

    feat: crear clase Estudiante con atributos

--- app/src/main/java/Estudiante.java
+++ app/src/main/java/Estudiante.java
@@ -1,11 +1,11 @@
 import java.util.ArrayList;
 
 //atributos
-public class Main {
+public class Estudiante {
     private String codigo;
     private String nombre;
     private String apellido;
     private int edad;
     private int semestre;
-    
+
 }
